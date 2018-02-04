a5121b8d8e81d9152013e6b2439d542626d760b7
sharland@gmail.com <sharland@gmail.com>
2018-02-04 12:14:46 +0000

    does this work?

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello azure - how are you?'
+  return 'Hello azure - how are you doing today?'
 
 if __name__ == '__main__':
   app.run()
